abfe5417fb2734c132eac1d7c1dece2959ac5c58
Stanimir Dimitrov <stanchino@gmail.com>
2013-02-03 16:25:31 +0200

    Initial push

---
+++ Application/src/com/zaxsis/HelloWorld/HelloWorldActivity.java
@@ -0,0 +1,13 @@
+package com.zaxsis.HelloWorld;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class HelloWorldActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
